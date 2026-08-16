897bf7371f5097021810f6739317cc32116c6f45
parshant <parshantbisht656@gmail.com>
2026-08-16 17:30:57 +0530

    Python

---
+++ Python/main.py
@@ -0,0 +1,55 @@
+print("Hello world: ")
+
+# Comments
+""" hello
+ how 
+ are
+ you """
+print("------------------------------------------------------")
+# Variables
+sher="Hello world 12:"
+print(sher)
+
+print("------------------------------------------------------")
+#Data types
+
+a=-56
+b="Hello world"
+c=0.123333333
+d=9/7
+e= True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print("------------------------------------------------------")
+# String and its functions 
+
+f="a"
+print(ord(f))
+g="A"
+print(ord(g))
+
+ab="COLLEGE"
+print(ab[0],ab[-1])
+
+print(ab[1:7:2])
+
+print("------------------------------------------------------")
+# Type Conversion:-
+
+# String to int
+x= "12.5"
+y= float(x)    
+print("new y is ",y)
+print(type(y))
+
+print("------------------------------------------------------")
+a= 10
+b= True
+c=2+3j
+print(type(str(a)))
+print(type(str(b)))
+print(type(str(c)))
